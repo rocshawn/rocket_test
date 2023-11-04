fc3028e2afc643905de8602220b6a4884717b4a7
RocShawn <278331228@qq.com>
2023-11-04 14:38:38 +0800

    完成了tcp部分注释

--- rocket/net/tcp/net_addr.cc
+++ rocket/net/tcp/net_addr.cc
@@ -5,18 +5,21 @@
 
 namespace rocket {
 
-
+//定义静态函数 CheckValid ，用于检查传入的 addr 字符串是否是一个有效的IP地址
 bool IPNetAddr::CheckValid(const std::string& addr) {
+  //查找字符串 addr 中的 : 字符，如果找不到则返回 false
   size_t i = addr.find_first_of(":");
   if (i == addr.npos) {
     return false;
   }
+  //根据找到的 : 字符位置，拆分出IP地址和端口号
   std::string ip = addr.substr(0, i);
   std::string port = addr.substr(i + 1, addr.size() - i - 1);
+  //如果IP地址或端口号为空，则返回 false
   if (ip.empty() || port.empty()) {
     return false;
   }
-
+  //将端口号字符串转换为整数，并检查它是否在合法的范围内
   int iport = std::atoi(port.c_str());
   if (iport <= 0 || iport > 65536) {
     return false;
@@ -25,16 +28,19 @@ bool IPNetAddr::CheckValid(const std::string& addr) {
   return true;
 
 }
-
+//定义 IPNetAddr 的构造函数，接受IP地址字符串和端口号作为参数
 IPNetAddr::IPNetAddr(const std::string& ip, uint16_t port) : m_ip(ip), m_port(port) {
+  //将 m_addr 结构体的内存清零
   memset(&m_addr, 0, sizeof(m_addr));
-
+  //为 m_addr 结构体设置IP地址、端口号和协议族
   m_addr.sin_family = AF_INET;
   m_addr.sin_addr.s_addr = inet_addr(m_ip.c_str());
   m_addr.sin_port = htons(m_port);
 }
-  
+
+//定义 IPNetAddr 的构造函数，接受一个 addr 字符串，它应该包含IP地址和端口号
 IPNetAddr::IPNetAddr(const std::string& addr) {
+  //解析传入的 addr 字符串，提取IP地址和端口号
   size_t i = addr.find_first_of(":");
   if (i == addr.npos) {
     ERRORLOG("invalid ipv4 addr %s", addr.c_str());
@@ -42,7 +48,7 @@ IPNetAddr::IPNetAddr(const std::string& addr) {
   }
   m_ip = addr.substr(0, i);
   m_port = std::atoi(addr.substr(i + 1, addr.size() - i - 1).c_str());
-
+  //为 m_addr 结构体设置IP地址、端口号和协议族
   memset(&m_addr, 0, sizeof(m_addr));
   m_addr.sin_family = AF_INET;
   m_addr.sin_addr.s_addr = inet_addr(m_ip.c_str());
@@ -50,39 +56,42 @@ IPNetAddr::IPNetAddr(const std::string& addr) {
 
 }
 
+//定义 IPNetAddr 的构造函数，接受一个 sockaddr_in 结构体作为参数
 IPNetAddr::IPNetAddr(sockaddr_in addr) : m_addr(addr) {
+  //从结构体中提取IP地址和端口号
   m_ip = std::string(inet_ntoa(m_addr.sin_addr));
   m_port = ntohs(m_addr.sin_port);
 
 }
-
+//返回一个指向 m_addr 的 sockaddr 类型的指针
 sockaddr* IPNetAddr::getSockAddr() {
   return reinterpret_cast<sockaddr*>(&m_addr);
 }
-
+//返回 m_addr 的大小
 socklen_t IPNetAddr::getSockLen() {
   return sizeof(m_addr);
 }
-
+//返回IP地址的协议族(IPv4)
 int IPNetAddr::getFamily() {
   return AF_INET;
 }
-
+//将IP地址和端口号转换为字符串
 std::string IPNetAddr::toString() {
+  //构造并返回IP地址和端口号的字符串表示
   std::string re;
   re = m_ip + ":" + std::to_string(m_port);
   return re;
 }
-
+//检查当前对象中的IP地址和端口号是否有效
 bool IPNetAddr::checkValid() {
   if (m_ip.empty()) {
     return false;  
   }
-
+  //检查端口号是否在合法范围内
   if (m_port < 0 || m_port > 65536) {
     return false;
   }
-
+  //检查IP地址是否是一个有效的IPv4地址
   if (inet_addr(m_ip.c_str()) == INADDR_NONE) {
     return false;
   }
--- rocket/net/tcp/tcp_buffer.cc
+++ rocket/net/tcp/tcp_buffer.cc
@@ -6,49 +6,55 @@
 namespace rocket {
 
 
-
+//定义 TcpBuffer 类的构造函数，接受一个整数 size 作为参数，设置成员变量 m_size 的值并调整 m_buffer 的大小。
 TcpBuffer::TcpBuffer(int size) : m_size(size) {
   m_buffer.resize(size);
 }
 
+//析构函数
 TcpBuffer::~TcpBuffer() {
 
 }
 
-// 返回可读字节数
+// 返回可读字节数,通过计算写索引和读索引的差来得到的
 int TcpBuffer::readAble() {
   return m_write_index - m_read_index;
 }
 
-// 返回可写的字节数
+// 返回可写的字节数,通过计算缓冲区的大小和写索引的差来得到的
 int TcpBuffer::writeAble() {
   return m_buffer.size() - m_write_index;
 }
 
+//读索引的值
 int TcpBuffer::readIndex() {
   return m_read_index;
 }
 
+//返回写索引的值
 int TcpBuffer::writeIndex() {
   return m_write_index;
 }
 
+//将给定的 buf 写入缓冲区，size 是 buf 的长度
 void TcpBuffer::writeToBuffer(const char* buf, int size) {
+  //如果要写入的数据大小超过了可写空间，就调整缓冲区的大小
   if (size > writeAble()) {
     // 调整 buffer 的大小，扩容
     int new_size = (int)(1.5 * (m_write_index + size));
     resizeBuffer(new_size);
   }
+  //将数据从 buf 拷贝到缓冲区，并调整写索引
   memcpy(&m_buffer[m_write_index], buf, size);
   m_write_index += size; 
 }
 
-
+//定义一个函数，从缓冲区中读取数据到给定的 re，size 是期望读取的字节数
 void TcpBuffer::readFromBuffer(std::vector<char>& re, int size) {
   if (readAble() == 0) {
     return;
   }
-
+  //计算实际读取的字节数，然后从缓冲区中拷贝到临时向量，再将临时向量的内容和给定的向量交换，并调整读索引及缓冲区
   int read_size = readAble() > size ? size : readAble();
 
   std::vector<char> tmp(read_size);
@@ -60,8 +66,9 @@ void TcpBuffer::readFromBuffer(std::vector<char>& re, int size) {
   adjustBuffer();
 }
 
-
+//用于调整缓冲区的大小
 void TcpBuffer::resizeBuffer(int new_size) {
+  //创建一个新的临时缓冲区，然后从原始缓冲区中拷贝数据到临时缓冲区，并交换两个缓冲区的内容。调整读、写索引
   std::vector<char> tmp(new_size);
   int count = std::min(new_size, readAble());
   
@@ -73,11 +80,13 @@ void TcpBuffer::resizeBuffer(int new_size) {
 
 }
 
-
+//用于调整缓冲区，确保未读的数据移动到缓冲区的开始位置
 void TcpBuffer::adjustBuffer() {
+  //如果读索引小于缓冲区大小的1/3，直接返回
   if (m_read_index < int(m_buffer.size() / 3)) {
     return;
   }
+  //创建一个新的缓冲区，然后从原始缓冲区中拷贝未读数据到新缓冲区，并交换两个缓冲区的内容。调整读、写索引
   std::vector<char> buffer(m_buffer.size());
   int count = readAble();
 
@@ -89,7 +98,9 @@ void TcpBuffer::adjustBuffer() {
   buffer.clear();
 }
 
+//移动读索引
 void TcpBuffer::moveReadIndex(int size) {
+  //计算新的读索引，如果它超出了缓冲区的大小，记录错误并返回。否则设置新的读索引并调整缓冲区
   size_t j = m_read_index + size;
   if (j >= m_buffer.size()) {
     ERRORLOG("moveReadIndex error, invalid size %d, old_read_index %d, buffer size %d", size, m_read_index, m_buffer.size());
@@ -99,6 +110,7 @@ void TcpBuffer::moveReadIndex(int size) {
   adjustBuffer();
 }
 
+//移动写索引
 void TcpBuffer::moveWriteIndex(int size) {
   size_t j = m_write_index + size;
   if (j >= m_buffer.size()) {
